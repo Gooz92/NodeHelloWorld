3b7d86f47e63edf2ca6456d5a35b42b93bc629f0
Anton <kurguzenkov@gmail.com>
2024-06-21 14:33:11 +0300

    change port

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(80, function () {
-  console.log('Example app listening on port 80!');
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
